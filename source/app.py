00f9923738d8ac28a4f0a7527de4137f1d4e9ff1
Sepul014 <92600381+Sepul014@users.noreply.github.com>
2023-11-05 22:19:09 -0800

    Update app.py

--- source/app.py
+++ source/app.py
@@ -6,8 +6,9 @@ import sys
 import datetime
 import bcrypt
 import traceback
-
-from tools.eeg import get_head_band_sensor_object
+import json
+#from tools.eeg import get_head_band_sensor_object
+from tools.eeg import simulate_brain_bit_signal_data, on_brain_bit_signal_data_received
 
 
 from db_con import get_db_instance, get_db
@@ -33,8 +34,9 @@ def init_new_env():
     if 'db' not in g:
         g.db = get_db()
 
-    if 'hb' not in g:
-        g.hb = get_head_band_sensor_object()
+    if 'hb' not in g:#
+        g.hb = None
+      #  g.hb = get_head_band_sensor_object()#
 
     #g.secrets = get_secrets()
     #g.sms_client = get_sms_client()
@@ -68,8 +70,6 @@ def exec_secure_proc(proc_name):
 
     return resp
 
-
-
 @app.route("/open_api/<proc_name>",methods=['GET', 'POST'])
 def exec_proc(proc_name):
     logger.debug(f"Call to {proc_name}")
@@ -91,7 +91,44 @@ def exec_proc(proc_name):
 
     return resp
 
+def get_database_info():
+    with get_db() as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM brain")
+        rows = cursor.fetchall()
+
+        results = []
+        for row in rows:
+            try:
+                data = json.loads(row[2])
+            except json.JSONDecodeError:
+                data = row[2]
+            results.append(dict(movieID=row[0], data=data))
+    return results
+    
+@app.route("/database_info")
+def database_info():
+    try:
+
+        results = get_database_info() 
+        return json_response(data=results)
+    except Exception as e:
+        logger.error(f"Error fetching database info: {e}")
+        return json_response(status_=500, data={"error": str(e)})
+    
+
+@app.route("/simulate_eeg_data")
+def simulate_eeg_data():
+    try:
+        simulated_data = simulate_brain_bit_signal_data()
+        
+        on_brain_bit_signal_data_received(None, simulated_data)
+        
+        return json_response(data={"message": "Simulated EEG data generated and stored successfully"})
+    except Exception as e:
+        logger.error(f"Error simulating EEG data: {e}")
+        return json_response(status_=500, data={"error": str(e)})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
